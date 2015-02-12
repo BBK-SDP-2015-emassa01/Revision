066e6b518c480ea1e02a83f93b64aee27661af4d
Esha Massand <ubemas001@mail.bbk.ac.uk>
2015-02-12 19:36:34 +0000

    revise Generics java

---
+++ src/main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Esha
+ */
+public class main {
+    
+    public static void main (String[] args){
+    
+        Primes newPrime = new Primes();
+        newPrime.primeCalc();
+    
+        
+//    String string = "hi my name is";
+//    Integer number = 20;
+//    int num = 20;
+//    
+//    BoxT<String> boxString = new BoxT<String>();//generics
+//    boxString.set(string);
+//    System.out.println(boxString.get());
+//    
+//    BoxT boxNum = new BoxT();//raw type
+//    boxNum.set(number);
+//    System.out.println(boxNum.get());
+//    
+//    Box boxint = new Box();
+//    boxint.set(num);
+//    System.out.println(boxint.get());
+}
+    
+}
